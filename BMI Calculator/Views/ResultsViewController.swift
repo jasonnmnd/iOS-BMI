f155eaec0a7fb9b81dcc2fcb0e66e8512315ff4b
Jason Dong <jasondong@Jasons-MacBook-Pro.local>
2021-07-20 22:52:13 -0400

    Completed BMI

--- BMI Calculator/Views/ResultsViewController.swift
+++ BMI Calculator/Views/ResultsViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ResultsViewController: UIViewController {
     
     var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
     
     @IBOutlet weak var adviceLabel: UILabel!
     @IBOutlet weak var bmiLabel: UILabel!
@@ -23,6 +25,8 @@ class ResultsViewController: UIViewController {
         super.viewDidLoad()
         
         bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
         // Do any additional setup after loading the view.
     }
     
---
+++ BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Jason Dong on 7/20/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
